8b0ef7a2b999734241ba0a02c0a9239b7e4cc174
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-21 10:38:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+get '/name' do 
+    "My name is Jacob"
+  end
+
+  get '/hometown' do 
+    "My hometown is Virginia Water"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Power of Love (Back to the Future)"
+  end
 end
