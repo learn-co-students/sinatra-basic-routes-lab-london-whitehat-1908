77846b212885dfba5dfe88464c22836efe417fba
joebelton <joebelton@live.co.uk>
2019-08-25 23:57:34 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Joe"
+    end
+
+    get '/hometown' do
+        "My hometown is Loughton"
+      end
+
+    get '/favorite-song' do
+        "My favorite song is Descpacito"
+    end
 end
